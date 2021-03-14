7e2fa932921a4fec56aba1c26bc855563ad2c7d5
Andrew Ogilvie <primalorb@gmail.com>
2021-03-14 13:22:01 -0400

    add touch punch controls

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,14 +176,6 @@ $('.card .list-group').sortable({
         })
     });
 
-    console.log(tempArr)
- 
-
-
-
-
-
-
     // trim down list id to match object prop
   var arrName = $(this)
     .attr('id')
@@ -192,9 +184,23 @@ $('.card .list-group').sortable({
     // update array on tasks object and save
   tasks[arrName] = tempArr;
   saveTasks()
-
-
   },
+})
+
+$('#trash').droppable({
+  accept: '.card .list-group-item',
+  tolerance: 'touch',
+  drop: function( event, ui ) {
+    console.log('drop')
+    ui.draggable.remove()
+  },
+  over: function( event, ui ) {
+    console.log('over')
+  },
+  out: function( event, ui ) {
+    console.log('out')
+  },
+  
 })
 
 
